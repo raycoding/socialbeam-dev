0f137cc2b18f8de29e64ab9bbb559e89589c3f81
ray <shuddhashil.ray@infibeam.net>
2014-03-06 22:43:16 +0530

    KBD 1.0

---
+++ Gemfile
@@ -0,0 +1,17 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.13'
+gem 'mysql2','0.3.15'
+gem 'execjs','~>2.0.2'
+gem 'therubyracer', '~>0.12.1', :platforms => :ruby
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'uglifier', '>= 1.0.3'
+end
+gem 'jquery-rails','~>3.1.0'
+gem 'jquery-ui-rails','~>4.0.5'
+gem 'bcrypt-ruby','~>3.0.1'
+group :development do
+	gem 'hirb','~>0.7.1'
+	gem 'interactive_editor','~>0.0.10'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Killedbydoozy::Application.config.secret_token = 'f888daa88124a10632682da0bfde054be7493e54f0ec1958c6132c267ea9d20dca6f09f603cde4d8f50ef8a076e6f4c1a3ab21cc10f1128d8696672e2e2ce7fc'
